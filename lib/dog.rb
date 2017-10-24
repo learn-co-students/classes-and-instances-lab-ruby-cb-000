7a8002568c96ab86be56d15688af3e5824aa307c
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-10-24 04:11:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class dog
+end
+fido = dog.new
+fido
+
+snoopy = dog.new
+snoopy
+
+lassie = dog.new
+lassie
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class person
+end
+
+adele_goldberg = person.new
+adele_goldberg
+
+alan_kay = person.new
+alan_kay
